898d42cd177d570d8d34b95e745f023efe6d6fc2
LambicJaune <thehopman@outlook.com>
2025-06-02 17:12:35 +0200

    Added pokemonList loop with name + height
    Loop iterating over each item of the pokemonList to display the Pokémon's name + height, and add a comment about the size when height is over 0.6.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 let pokemonList = [
 {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
 {name: 'Charmander', height: 0.6, types: ['fire']},
-{name: 'Squirtle', height: 0.5, type: ['water']},
-];
+{name: 'Squirtle', height: 0.5, types: ['water']},
+]; 
+
+/* Loop iterating all items of the pokemonList array displaying name and height, with an added conditional displaying an extra comment to any items with a heigth over 0.6 */
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ');
+  if (pokemonList[i].height > 0.6) {
+    document.write('- Wow, that\'s big!');
+  }
+    document.write('<br>');
+}
